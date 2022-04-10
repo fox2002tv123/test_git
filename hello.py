4be4f76f32fbe768ff330fb9a79661eaad146b34
fox2002tv123 <85301925@qq.com>
2022-04-10 17:48:20 +0800

    bugFix上的提交

---
+++ hello.py
@@ -0,0 +1,11 @@
+'''
+Author: bob
+Date: 2022-04-10 17:46:21
+LastEditors: bob
+LastEditTime: 2022-04-10 17:46:22
+FilePath: \test_git\hello.py
+Description: 测试用
+
+Copyright (c) 2022 by bob, All Rights Reserved. 
+'''
+print('hello')
